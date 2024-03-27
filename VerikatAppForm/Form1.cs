a944d78b9f2c1d601e5c125b173cb2648fe3d351
Alparslan AKBAŞ <bybluestht@gmail.com>
2024-03-27 16:12:04 +0300

    Proje dosyası ekle.

---
+++ VerikatAppForm/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace VerikatAppForm
+{
+    public partial class Form1 : Form
+    {
+        private string logFilePath = $"C:\\Users\\byblu\\Desktop\\VeriketApp\\VeriketAppService\\bin\\Debug\\Logs\\VeriketAppTest_{DateTime.Now.Date.ToString("yyyy-MM-dd")}.txt";
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataGrid();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeDataGrid()
+        {
+            // DataGrid sütunlarını tanımla
+            dataGridView1.Columns.Add("Tarih", "Tarih");
+            dataGridView1.Columns.Add("BilgisayarAdi", "Bilgisayar Adı");
+            dataGridView1.Columns.Add("KullaniciAdi", "Kullanıcı Adı");
+
+            // Sütunları genişlet
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                column.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            }
+        }
+
+        private void ReadAndPopulateDataGrid(string filePath)
+        {
+            try
+            {
+                // CSV dosyasındaki satırları oku
+                List<string> lines = File.ReadAllLines(filePath).ToList();
+
+                // Her bir satırı DataGrid'e ekle
+                foreach (string line in lines)
+                {
+                    string[] values = line.Split(',');
+                    dataGridView1.Rows.Add(values);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata oluştu: {ex.Message}");
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // Log dosyasındaki satırları oku ve DataGrid'e ekle
+            ReadAndPopulateDataGrid(logFilePath);
+        }
+    }
+}
---
+++ VeriketAppService/Service1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace VeriketAppService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Timer timer;
+        // Kod tekrarına düşmemek için sabit bir değişken oluşturuyoruz
+        private string logData;
+
+        public Service1()
+        {
+            InitializeComponent();
+            logData = $"{DateTime.Now}, {Environment.MachineName}, {GetLoggedInUser()}";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Timerı başlatıyoruz
+            timer = new Timer();
+            WriteToFile("Servis Başlatıldı: " + logData);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000; // 5 saniyede bir çalışacak
+            timer.Enabled = true;
+            OnElapsedTime(null, null); // Servis başladığında ilk logu yaz
+        }
+
+        protected override void OnStop()
+        {
+            // Timerı durduruyoruz
+            WriteToFile("Servis Durduruldu");
+        }
+
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            // Timer çalıştığında şu anki zamanı, makine adını ve kullanıcı adını loga yazıyoruz
+            WriteToFile("Servis Çalışıyor: " + logData);
+        }
+
+        public void WriteToFile(string Message)
+        {
+            // Dosyaya yazma işlemi
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = Path.Combine(path, "VeriketAppTest_" + DateTime.Now.Date.ToString("yyyy-MM-dd") + ".txt");
+
+            // Dosya varsa sonuna ekleme yapıyoruz
+            using (StreamWriter sw = File.AppendText(filepath))
+            {
+                sw.WriteLine(Message);
+            }
+        }
+
+        private string GetLoggedInUser()
+        {
+            IntPtr buffer;
+            int sessionId = -1; // -1 oturum açık olan kullanıcıyı almak için kullanılır
+            int bytesReturned;
+
+            if (WTSQuerySessionInformation(IntPtr.Zero, sessionId, WTS_INFO_CLASS.WTSUserName, out buffer, out bytesReturned))
+            {
+                string loggedInUser = Marshal.PtrToStringAnsi(buffer);
+                WTSFreeMemory(buffer);
+                return loggedInUser;
+            }
+            else
+            {
+                return "Kullanıcı Bulunamadı";
+            }
+        }
+
+        [DllImport("WTSAPI32.DLL", SetLastError = true)]
+        static extern bool WTSQuerySessionInformation(
+            IntPtr hServer,
+            int sessionId,
+            WTS_INFO_CLASS wtsInfoClass,
+            out IntPtr ppBuffer,
+            out int pBytesReturned);
+
+        [DllImport("WTSAPI32.DLL", SetLastError = true)]
+        static extern void WTSFreeMemory(IntPtr pointer);
+
+        enum WTS_INFO_CLASS
+        {
+            WTSUserName = 5
+        }
+    }
+}
